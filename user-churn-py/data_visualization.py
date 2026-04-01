903a64fdd477af66d602902d618f83241e489fb3
Eungwi Jo <jeg0330@gmail.com>
2026-04-02 02:39:41 +0900

    feat: 피처 중요도 시각화 추가

--- user-churn-py/data_visualization.py
+++ user-churn-py/data_visualization.py
@@ -36,3 +36,19 @@ def visualize_scatter_matrix(col_list, target_col, df, font_size=1.5):
     sns.pairplot(data=df, vars=col_list, hue=target_col, palette='bright', markers='+')
 
     plt.show()
+
+
+def visualize_feature_importance(model, feature_names, font_size=1.5):
+    plt.clf()
+    plt.figure(figsize=(12, 8))
+    sns.set(font_scale=font_size)
+
+    importances = model.feature_importances_
+    indices = importances.argsort()
+
+    plt.barh(range(len(indices)), importances[indices])
+    plt.yticks(range(len(indices)), [feature_names[i] for i in indices])
+    plt.xlabel('Feature Importance')
+    plt.title('Random Forest Feature Importance')
+    plt.tight_layout()
+    plt.show()
